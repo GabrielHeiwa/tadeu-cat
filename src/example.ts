be3d078e46278c95b365c5d1b9dfc857776f01ac
unknown <heiwa.gabriel@gmail.com>
2021-09-09 11:19:10 -0300

    add example file

---
+++ src/example.ts
@@ -0,0 +1,11 @@
+import { Client } from "whatsapp-web.js";
+import { generate } from "qrcode-terminal";
+
+const client = new Client({
+    puppeteer: {
+        args: ["--no-sandbox", '--disable-setuid-sandbox']
+    }
+});
+
+client.initialize();
+client.on("qr", qr => generate(qr));
