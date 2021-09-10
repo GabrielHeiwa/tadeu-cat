fe94babd612e3dd3d24040462f48b384e964b5e7
unknown <heiwa.gabriel@gmail.com>
2021-09-10 16:37:51 -0300

    Add: Try catch to example file to capture errors

--- dist/example.js
+++ dist/example.js
@@ -2,15 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const qrcode_terminal_1 = require("qrcode-terminal");
 const whatsapp_web_js_1 = require("whatsapp-web.js");
-const client = new whatsapp_web_js_1.Client({
-    puppeteer: {
-        headless: false,
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--disable-extensions",
-        ],
-    },
-});
-client.initialize();
-client.on("qr", (qr) => qrcode_terminal_1.generate(qr, { small: true }));
+try {
+    const client = new whatsapp_web_js_1.Client({
+        puppeteer: {
+            headless: false,
+            args: ["--disable-setuid-sandbox", "--no-sandbox"],
+        },
+    });
+    client.initialize();
+    client.on("qr", (qr) => qrcode_terminal_1.generate(qr, { small: true }));
+}
+catch (err) {
+    console.log(err);
+}
