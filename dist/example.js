a6d4c3009ecbe40f6d68f8479295fde62ace7c4d
unknown <heiwa.gabriel@gmail.com>
2021-09-10 16:34:23 -0300

    ADD: example file to whatsapp-web browser

--- dist/example.js
+++ dist/example.js
@@ -1,16 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const whatsapp_web_js_1 = require("whatsapp-web.js");
 const qrcode_terminal_1 = require("qrcode-terminal");
-const client = new whatsapp_web_js_1.Client({
-    puppeteer: {
-        args: ["--no-sandbox", '--disable-setuid-sandbox']
-    }
-});
-try {
+const whatsapp_web_js_1 = require("whatsapp-web.js");
+(async () => {
+    const client = new whatsapp_web_js_1.Client({
+        puppeteer: {
+            headless: false,
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--disable-extensions",
+            ],
+        }
+    });
     client.initialize();
-    client.on("qr", qr => qrcode_terminal_1.generate(qr));
-}
-catch (err) {
-    console.error(err);
-}
+    client.on("qr", qr => qrcode_terminal_1.generate(qr, { small: true }));
+})();
